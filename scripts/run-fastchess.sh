7f89e4813f1b4ae479d078144c05941f222bc6e8
Danny (craptop) <hammerapi@gmail.com>
2024-08-13 21:07:06 -0600

    chore: made fastchess script run SPRT

--- scripts/run-fastchess.sh
+++ scripts/run-fastchess.sh
@@ -9,5 +9,6 @@ fastchess \
 		-engine cmd=$engine1 name=$name1 \
 	    -engine cmd=$engine2 name=$name2 \
 	    -openings file=epd/8moves_v3.epd format=epd order=random \
-	    -each tc=8+0.08 -rounds 500 -repeat -concurrency 1 -log file=fastchess.log
-	    # -each tc=8+0.08 -rounds 500 -repeat -concurrency 6
+	    -each tc=8+0.08 -rounds 50000 -repeat -concurrency $(nproc) \
+		-sprt elo0=0 elo1=5 alpha=0.05 beta=0.05
+	    #-each tc=8+0.08 -rounds 500 -repeat -concurrency 1 -log file=fastchess.log
